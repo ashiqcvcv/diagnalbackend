1a05eefc9a1da52bda2959e160e1a110ccbeb155
ashiqcvcv <ashiqc23@gmail.com>
2021-10-08 21:24:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+const cors = require('cors');
+
+app.use(cors())
+
+app.get('/', async (req, res) => {
+    var pageno = req.query.no;
+    if ( !pageno ) {
+        res.send({});
+        return;
+    }
+    var data = async () => {
+        try {
+            return await require(`./API/CONTENTLISTINGPAGE-PAGE${pageno}.json`)
+        } catch (error) {
+            return {};
+        }
+    }
+
+    var out = await data();
+    res.json(out);
+})
+
+
+
+
+var server = app.listen(3001, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("HOSTED IN " + host + ' ' + port);
+})
